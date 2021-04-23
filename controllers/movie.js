d6180ccd7503226bd3cb77b26ce9a0435c0a58f1
Chari16 <bhaveshchari1997@gmail.com>
2021-04-23 15:54:56 +0530

    crawling data from imbd website using puppeteer and writing records in a json file done

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
+const puppeteer = require("puppeteer");
 
-list = async(req, res, next) => {
+list = (req, res, next) => {
     try {
         fs.readFile("movies.json", "utf-8", (err, data) => {
             if (err) throw err;
@@ -16,6 +17,50 @@ list = async(req, res, next) => {
     }
 }
 
+crawl = async (req, res, next) => {
+    try {
+        const url = "https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating";
+
+        const browser = await puppeteer.launch();
+
+        const page = await browser.newPage();
+        
+        await page.goto(url);
+
+        const movies = await page.evaluate(async() => {
+            const elements = Array.from(document.querySelectorAll(".lister-item"))
+            return elements.map((movie) => (
+                {
+                    title: movie.querySelector(".lister-item-content > .lister-item-header > a").innerText,
+                    genre: movie.querySelector(".lister-item-content > .text-muted > .genre").innerText,
+                    description: movie.querySelector(".lister-item-content > p[class='text-muted']").innerText,
+                    director: movie.querySelector(".lister-item-content > p:nth-child(5) a")?.innerText,
+                    year: movie.querySelector(".lister-item-content > .lister-item-header > .lister-item-year").innerText,
+                    certificate: movie.querySelector(".lister-item-content > .text-muted > .certificate")?.innerText,
+                    rating: movie.querySelector(".lister-item-content > .ratings-bar > .ratings-imdb-rating > strong").innerText,
+                    votes: movie.querySelector(".lister-item-content > .sort-num_votes-visible > span:nth-child(2)").innerText,
+                    gross: movie.querySelector(".lister-item-content > .sort-num_votes-visible > span:nth-child(5)")?.innerText ?? null
+                }
+            ))
+        })
+        await browser.close();
+
+        fs.writeFileSync("./movies.json", JSON.stringify(movies),(err) => {
+            throw err;
+        });
+
+        res.status(200)
+        .json({
+            success: true,
+            message: "Data crawled successfully"
+        })
+    }
+    catch(e) {
+        next(e)
+    }
+}
+
 module.exports = {
-    list
+    list,
+    crawl
 }
--- routes/movie.js
+++ routes/movie.js
@@ -4,5 +4,6 @@ const movieController = require('../controllers/movie');
 const router = express.Router()
 
 router.get('/', movieController.list)
+router.get('/crawl', movieController.crawl)
 
 module.exports = router;
